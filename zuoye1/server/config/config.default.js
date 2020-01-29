6005defb7c5d33b7d3facb039d83cdb205474a86
313138958 <313138958@qq.com>
2020-01-29 15:40:09 +0800

    updata

--- zuoye1/server/config/config.default.js
+++ zuoye1/server/config/config.default.js
@@ -5,6 +5,7 @@
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
+const witchlist = require('./witchlist')
 module.exports = appInfo => {
   /**
    * built-in config
@@ -16,8 +17,8 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1578307898534_8515';
 
   // add your middleware config here
-  config.middleware = [];
-
+  config.middleware = ['userJwt'];
+  config.userJwt = witchlist
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
